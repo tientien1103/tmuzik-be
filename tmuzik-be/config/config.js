e7f314c2ac5ecd26c0c04159e6073664a003db15
Tien Le <124553193+tientien1103@users.noreply.github.com>
2023-08-04 17:18:31 +0700

    Delete tmuzik-be directory

--- tmuzik-be/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  user: "le.nttien99@gmail.com",
-  clientId:
-    "242306592177-mevie8rad6e8bopub15183u4ol7il1o0.apps.googleusercontent.com",
-  clientSecret: "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",
-  refreshToken:
-    "1//04PZ7gR0bs1LcCgYIARAAGAQSNwF-L9Ir2H_sWBTFr7WMzl5JQuxhX1JGd4Tj7LAhAgxQS10fb5DCwQW0AbYZTH8rUAvokKTHDOE",
-};
