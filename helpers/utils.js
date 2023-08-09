3b276a7a7ff298e242719d005a3012c5100dc04a
Tien Le <124553193+tientien1103@users.noreply.github.com>
2023-08-09 16:22:03 +0700

    Update utils.js

--- helpers/utils.js
+++ helpers/utils.js
@@ -15,9 +15,7 @@ class AppError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.errorType = errorType;
-    // all errors using this class are operational errors.
     this.isOperational = true;
-    // create a stack trace for debugging (Error obj, void obj to avoid stack polution)
     Error.captureStackTrace(this, this.constructor);
   }
 }
